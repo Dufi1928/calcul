6ceaea985a103c6f8f3bdeaad6712bb698943601
Ivan <83720387+Dufi1928@users.noreply.github.com>
2025-03-31 11:20:54 +0200

    feat: expended tests

---
+++ calculatrice.js
@@ -0,0 +1,34 @@
+// calculatrice.js
+class Calculatrice {
+    constructor() {
+        this.historique = [];
+    }
+
+    add(a, b) {
+        const result = a + b;
+        this.historique.push({ operation: 'addition', a, b, result });
+        return result;
+    }
+
+    subtract(a, b) {
+        const result = a - b;
+        this.historique.push({ operation: 'soustraction', a, b, result });
+        return result;
+    }
+
+    multiply(a, b) {
+        const result = a * b;
+        this.historique.push({ operation: 'multiplication', a, b, result });
+        return result;
+    }
+
+    getHistorique() {
+        return this.historique;
+    }
+
+    clearHistorique() {
+        this.historique = [];
+    }
+}
+
+module.exports = Calculatrice;
